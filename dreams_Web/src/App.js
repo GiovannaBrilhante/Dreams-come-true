8f35eac353a55a49f7cd9066d0db96314b83b786
Gabriel Santana <gabriel04.ok@gmail.com>
2022-11-11 15:59:28 -0300

    🕵️ | ESLint Integration

--- dreams_Web/src/App.js
+++ dreams_Web/src/App.js
@@ -1,23 +1,23 @@
-import "./App.css";
+import "./App.css"
 
-import Logo from "./components/templates/Logo";
-import Menu from "./components/templates/Menu";
-import Footer from "./components/templates/Footer";
+import Logo from "./components/templates/Logo"
+import Menu from "./components/templates/Menu"
+import Footer from "./components/templates/Footer"
 
-import Rotas from "./Rotas";
-import { BrowserRouter } from "react-router-dom";
+import Rotas from "./Rotas"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="App">
+            <div className='App'>
                 <Logo />
                 <Menu />
                 <Rotas />
                 <Footer />
             </div>
         </BrowserRouter>
-    );
+    )
 }
 
-export default App;
+export default App
