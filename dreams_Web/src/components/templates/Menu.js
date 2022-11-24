6d782a5e40a7cd96d28f8bdfb8de48f1c42cb97b
Gabriel Santana <gabriel04.ok@gmail.com>
2022-11-23 21:34:11 -0300

    [+] Adicionado codigo ausente

--- dreams_Web/src/components/templates/Menu.js
+++ dreams_Web/src/components/templates/Menu.js
@@ -14,18 +14,8 @@ export default function Menu(_props) {
             <Link to="/carometro">
                 Carômetro
             </Link>
-<<<<<<< HEAD
-
-            <Link to="/login">
-                Registrar-se
-            </Link>
-
-            <Link to="/carometro">
-                Entrar
-=======
             <Link to="/usuarios">
                 Usuários
->>>>>>> 30e8c70aa4aa7303d5eccd3bc0a60a208e53b203
             </Link>
         </nav>
     )
