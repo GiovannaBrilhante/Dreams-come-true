f97353117f81d8770318967211b7298c38a33293
Giovanna Brilhante <bricamagi@gmail.com>
2022-11-24 00:28:49 -0300

    Login/Logout

--- dreams_Web/src/Rotas.js
+++ dreams_Web/src/Rotas.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import CrudRestaurante from "./components/CrudRestaurante/CrudRestaurante"
 import NotFound from "./components/NotFound/NotFound"
@@ -6,15 +6,39 @@ import CrudFilme from "./components/CrudFilme/CrudFilme"
 import CrudCarometro from "./components/CrudCarometro/CrudCarometro"
 import Home from "./components/Home/body/Home"
 import CrudUsuario from "./components/CrudUsuario/CrudUsuario"
+import AuthService from "./services/AuthService"
+import Login from "./components/Login/Login"
+import Logout from "./components/Logout/Logout"
 
 export default function Rotas() {
+
+    const [currentUser, setCurrentUser] = useState(undefined)
+
+    useEffect(()=> {
+        const user = AuthService.getCurrentUser()
+        if(user) {
+            setCurrentUser(user)
+        }
+    }, [])
+
     return (
+
         <Routes>
             <Route exact path='/' element={ <Home/> } />
-            <Route path='/restaurantes' element={<CrudRestaurante/>} />
+            {currentUser ? (<Route path='/restaurantes' element={<CrudRestaurante/>} />) : (
+                <Route exact path='/restaurantes' 
+                    element={
+                        <Main title="Cadastro de Restaurantes">
+                            <div>Não autorizado! </div>
+                        </Main>} />
+            )}
+            
             <Route path="/filmes" element={<CrudFilme/>} />
             <Route path="/carometro" element={<CrudCarometro/>} />
             <Route path="/usuarios" element={<CrudUsuario/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/logout" element={<Logout/>} />
+            <Route path="*" to='/' />
 
             <Route /*Not found page*/ path='*' element={<NotFound/>} />
         </Routes>
