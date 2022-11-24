4595cee021b7c33170155176dd5ad677ea0b1102
Gabriel Santana <gabriel04.ok@gmail.com>
2022-11-24 00:33:56 -0300

    [+] Imagem da logo na home
    Co-authored-by: GIOVANNA BRILHANTE <bricamagi@gmail.com>

--- dreams_Web/src/components/Home/body/Home.js
+++ dreams_Web/src/components/Home/body/Home.js
@@ -1,23 +1,30 @@
-import React from "react"
+import React, { Component } from "react"
 import "./Home.css"
 
-export default function Home() {
-    return (
-
-        <section className="principal">
-            <div className="conteudo-principal">
-                <div className="conteudo-principal-logo">
-                    <div className="left conteudo-principal-subtitulo">
-                        <div>
-                            <span>Dreams Come True </span>
-                        </div>
-                        <div>
-                            <br />
-                            <p>Seja bem vindo ao Dreams Come True, o seu website e aplicativo de sonhos. Aqui você pode registrar seus sonhos e compartilhar com seus amigos. </p>
+export default class Home extends Component {
+    render() {
+        return (
+            <>
+                <main className="principal">
+                    <div className="conteudo-principal">
+                        <div className="conteudo-principal-logo">
+                            <div className="left conteudo-principal-subtitulo">
+                                <div>
+                                    <span>Dreams Come True </span>
+                                </div>
+                                <div>
+                                    <br />
+                                    <p>Seja bem vindo ao Dreams Come True, o seu website e aplicativo de sonhos. Aqui você pode registrar seus sonhos e compartilhar com seus amigos. </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
-    )
+
+                    <div className="conteudo-logo">
+                    </div>
+                </main>
+
+            </>
+        )
+    }
 }
