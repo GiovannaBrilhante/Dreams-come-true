4bdfcd866cd5767c441be1be34be7d10afb10fb4
Gabriel Santana <gabriel04.ok@gmail.com>
2022-11-29 03:51:55 -0300

    👨‍💻 | Redesign da seção de desenvolvedores

--- dreams_Web/src/components/pages/Home/Home.js
+++ dreams_Web/src/components/pages/Home/Home.js
@@ -5,6 +5,9 @@ import dev_gi from "../../../assets/images/dev_gi.png"
 import dev_ga from "../../../assets/images/dev_ga.jpeg"
 
 
+
+import { BsGithub } from "react-icons/bs"
+
 export default class Home extends Component {
 
     renderBanner() {
@@ -35,54 +38,105 @@ export default class Home extends Component {
                     <div className="devs-colletions w-dyn-list">
                         <div role="list" className="lista-dev w-dyn-items">
                             <div role="listitem" className="dev-card w-dyn-item">
-                                <div className="div-imagem-avatar">
-                                    <div className="avatar-cover">
-                                        <img alt="" loading="lazy" src={dev_gi} sizes="100vw" className="avatar" />
+                                <div className="wrapper">
+
+                                    <div className="div-imagem-avatar">
+                                        <div className="avatar-cover">
+                                            <img alt="" loading="lazy" src={dev_gi} sizes="100vw" className="avatar" />
+                                        </div>
+                                        <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
                                     </div>
-                                    <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
-                                </div>
-                                <div className="dev-infos">
-                                    <div className="dev-tag">
-                                        <p className="p-14 dev-cargo">Dona</p>
-                                    </div>
-                                    <h3 className="healine-18 dev-name">Giovanna Brilhante</h3>
-                                    <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
+                                    <a className="link" href="https://github.com/PuniGC" target="_blank" rel="noreferrer">
+                                        <div className="dev-infos">
+                                            <div className="dev-tag">
+                                                <p className="p-14 dev-cargo">Dona</p>
+                                            </div>
+                                            <h3 className="healine-18 dev-name">Giovanna Brilhante</h3>
+                                            <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
+                                        </div>
+                                    </a>
                                 </div>
                             </div>
-                            <div role="listitem" className="dev-card w-dyn-item">
-                                {
-                                    <div className="wrapper">
-                                        <div className="card front-face">
-                                            <div className="div-imagem-avatar">
-                                                <div className="avatar-cover">
-                                                    <img alt="" loading="lazy" src={dev_ga} sizes="100vw" className="avatar" />
-                                                </div>
-                                                <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
+
+
+                            { /*
+                                                        <div role="listitem" className="dev-card w-dyn-item">
+                                <div className="wrapper">
+                                    <div className="card front-face">
+                                        <div className="div-imagem-avatar">
+                                            <div className="avatar-cover">
+                                                <img alt="" loading="lazy" src={dev_gi} sizes="100vw" className="avatar" />
                                             </div>
+                                            <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
+                                        </div>
+                                        <a className="link" href="https://github.com/PuniGC" target="_blank" rel="noreferrer">
                                             <div className="dev-infos">
+                                                <div className="dev-tag">
+                                                    <p className="p-14 dev-cargo">Dona</p>
+                                                </div>
+                                                <h3 className="healine-18 dev-name">Giovanna Brilhante</h3>
+                                                <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                    <div id="container" className="card back-face">
+                                        <div className="info">
+                                            <div className="dev-infos">
+                                                <a className="link" href="https://github.com/PuniGC" target="_blank" rel="noreferrer">
+                                                    <div className="dev-tag-site">
+                                                        <p className="p-14 dev-site">GITHUB</p>
+                                                    </div>
+                                                    <h3 className="healine-18 dev-name">PuniGC</h3>
+                                                    <BsGithub />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+*/
+
+
+                            }
+
+
+
+                            <div role="listitem" className="dev-card w-dyn-item">
+                                <div className="wrapper">
+                                    <div className="card front-face">
+                                        <div className="div-imagem-avatar">
+                                            <div className="avatar-cover">
+                                                <img alt="" loading="lazy" src={dev_ga} sizes="100vw" className="avatar" />
+                                            </div>
+                                            <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
+                                        </div>
+                                        <a className="link" href="https://github.com/PuniGC" target="_blank" rel="noreferrer">
+                                            <div className="dev-infos">
+
                                                 <div className="dev-tag">
                                                     <p className="p-14 dev-cargo">Dono</p>
                                                 </div>
                                                 <h3 className="healine-18 dev-name">Gabriel Silva</h3>
                                                 <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
+
                                             </div>
-                                        </div>
-                                        <div className="card back-face">
-                                            <div className="info">
-                                                <div className="title">Desenvolvedor</div>
-                                                <p>Entre em contato comigo <br />por meio das redes</p>
-                                            </div>
-                                            <div>
-                                                <a href="https://www.instagram.com/theo_burgues/" target="_blank" rel="noreferrer">
-                                                    <i className="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                    <div id="container" className="card back-face">
+                                        <div className="info">
+                                            <div className="dev-infos">
+                                                <a className="link" href="https://github.com/PuniGC" target="_blank" rel="noreferrer">
+                                                    <div className="dev-tag-site">
+                                                        <p className="p-14 dev-site">GITHUB</p>
+                                                    </div>
+                                                    <h3 className="healine-18 dev-name">PuniGC</h3>
+                                                    <BsGithub />
                                                 </a>
-                                                <a href="https://github.com/taylorburgues" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a>
-                                                <a href="https://twitter.com/theo_burgues" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
-                                                <a href="https://mail.google.com/mail/u/0/?fs=1&to=theo_xone@outlook.com&tf=cm"
-                                                    target="_blank" rel="noreferrer"><i className="fa fa-envelope" aria-hidden="true"></i></a>
                                             </div>
                                         </div>
-                                    </div>}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
