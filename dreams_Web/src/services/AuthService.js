721f84892b4120ad34556b32c948fba7b14a31be
Giovanna Brilhante <bricamagi@gmail.com>
2022-11-29 01:05:26 -0300

    Testes login

--- dreams_Web/src/services/AuthService.js
+++ dreams_Web/src/services/AuthService.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:3000/api/Home/"
+const API_URL = "http://localhost:5006/"
 
 const login = (username, senha) => {
-    return axios.post(API_URL + "auth", {
+    return axios.post(API_URL + "login", {
         username,
         senha,
     })
