1644bd609848bac402cfab1886d4a23fc09a9203
Gabriel Santana <gabriel04.ok@gmail.com>
2022-11-29 01:12:12 -0300

    [+] To do developer back
    Co-authored-by: GIOVANNA BRILHANTE <bricamagi@gmail.com>

--- dreams_Web/src/components/pages/Home/Home.js
+++ dreams_Web/src/components/pages/Home/Home.js
@@ -50,52 +50,39 @@ export default class Home extends Component {
                                 </div>
                             </div>
                             <div role="listitem" className="dev-card w-dyn-item">
-                                <div className="div-imagem-avatar">
-                                    <div className="avatar-cover">
-                                        <img alt="" loading="lazy" src={dev_ga} sizes="100vw" className="avatar" />
-                                    </div>
-                                    <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
-                                </div>
-                                <div className="dev-infos">
-                                    <div className="dev-tag">
-                                        <p className="p-14 dev-cargo">Dono</p>
-                                    </div>
-                                    <h3 className="healine-18 dev-name">Gabriel Silva</h3>
-                                    <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
-                                </div>
-                                { /*
-                                <div className="wrapper">
-                                    <div className="card front-face">
-                                        <div className="div-imagem-avatar">
-                                            <div className="avatar-cover">
-                                                <img alt="" loading="lazy" src={dev_ga} sizes="100vw" className="avatar" />
+                                {
+                                    <div className="wrapper">
+                                        <div className="card front-face">
+                                            <div className="div-imagem-avatar">
+                                                <div className="avatar-cover">
+                                                    <img alt="" loading="lazy" src={dev_ga} sizes="100vw" className="avatar" />
+                                                </div>
+                                                <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
                                             </div>
-                                            <img src="https://global-uploads.webflow.com/61d83a2ebb0ae01ab96e841a/6380c3e02a3378ffbeb10627_faixa-speackers.svg" loading="lazy" alt="" />
-                                        </div>
-                                        <div className="dev-infos">
-                                            <div className="dev-tag">
-                                                <p className="p-14 dev-cargo">Dono</p>
+                                            <div className="dev-infos">
+                                                <div className="dev-tag">
+                                                    <p className="p-14 dev-cargo">Dono</p>
+                                                </div>
+                                                <h3 className="healine-18 dev-name">Gabriel Silva</h3>
+                                                <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
                                             </div>
-                                            <h3 className="healine-18 dev-name">Gabriel Silva</h3>
-                                            <p className="p-14 dev-about">CTO (Chief Technology Officer) <br /><strong>Dreams Lover</strong></p>
                                         </div>
-                                    </div>
-<div className="card back-face">
-                                        <div className="info">
-                                            <div className="title">Desenvolvedor</div>
-                                            <p>Entre em contato comigo <br />por meio das redes</p>
+                                        <div className="card back-face">
+                                            <div className="info">
+                                                <div className="title">Desenvolvedor</div>
+                                                <p>Entre em contato comigo <br />por meio das redes</p>
+                                            </div>
+                                            <div>
+                                                <a href="https://www.instagram.com/theo_burgues/" target="_blank" rel="noreferrer">
+                                                    <i className="fab fa-instagram"></i>
+                                                </a>
+                                                <a href="https://github.com/taylorburgues" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a>
+                                                <a href="https://twitter.com/theo_burgues" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
+                                                <a href="https://mail.google.com/mail/u/0/?fs=1&to=theo_xone@outlook.com&tf=cm"
+                                                    target="_blank" rel="noreferrer"><i className="fa fa-envelope" aria-hidden="true"></i></a>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <a href="https://www.instagram.com/theo_burgues/" target="_blank" rel="noreferrer">
-                                                <i className="fab fa-instagram"></i>
-                                            </a>
-                                            <a href="https://github.com/taylorburgues" target="_blank" rel="noreferrer"><i className="fa fa-github" style="font-size:15px"></i></a>
-                                            <a href="https://twitter.com/theo_burgues" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
-                                            <a href="https://mail.google.com/mail/u/0/?fs=1&to=theo_xone@outlook.com&tf=cm"
-                                                target="_blank" rel="noreferrer"><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                                        </div>
-                                    </div> 
-                                </div>*/}
+                                    </div>}
                             </div>
                         </div>
                     </div>
